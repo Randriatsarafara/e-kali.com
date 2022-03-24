e95359e7dbe913815ba9b130b2d59b7f46996424
Randriatsarafara <67787954+Randriatsarafara@users.noreply.github.com>
2022-03-24 09:55:43 +0100

    app-name

--- view/server.js
+++ view/server.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/<app-name>'));
+app.use(express.static(__dirname + '/dist/view'));
 app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+
-'/dist/<nom-app>/index.html') );});
+res.sendFile(path.join(__dirname+'/dist/view/index.html') );});
 app.listen(process.env.PORT || 8080);
