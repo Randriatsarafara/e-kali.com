a7a8d870132cf6ab5dab017ab9dbd4094c391c66
Randriatsarafara <67787954+Randriatsarafara@users.noreply.github.com>
2022-04-05 05:48:38 +0200

    Rename projet

--- server.js
+++ server.js
@@ -5,4 +5,4 @@ app.use(express.static(__dirname + '/dist/m1p9mean-fitahiana'));
 app.get('/*', function(req,res) {
   res.sendFile(path.join(__dirname+'/dist/m1p9mean-fitahiana/index.html'));
 });
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 80);
