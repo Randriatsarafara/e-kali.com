f6ad427b9f96431d32f9139904fc767d49eb23a6
Randriatsarafara <67787954+Randriatsarafara@users.noreply.github.com>
2022-04-05 05:26:24 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/kali'));
+app.use(express.static(__dirname + '/dist/m1p9mean-fitahiana'));
 app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname+'/dist/kali/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/m1p9mean-fitahiana/index.html'));
 });
 app.listen(process.env.PORT || 8080);
