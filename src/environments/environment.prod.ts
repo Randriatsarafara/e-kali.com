3211c8b785acfba8279a8dc09b10384abb2c2485
Randriatsarafara <67787954+Randriatsarafara@users.noreply.github.com>
2022-04-10 23:45:12 +0300

    Deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true
 };
-export const base_url_node = "http://localhost:3001";
+export const base_url_node = "https://api-e-kali.herokuapp.com";
+// export const base_url_node = "http://localhost:3001";
